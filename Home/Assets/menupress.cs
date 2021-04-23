ef6a7a7654898cf914662a070c43a6c085f411d4
Can Berk Sahin <Canberksahin97@gmail.com>
2021-04-23 03:57:21 +0100

    Main Menu Re-haul
    Made changes to main menu
    -changed from a 2D Menu to a 3D menu with moving environment
    - designed a new environment for the menu
    - added post processing to main menu
    - added a main menu script to setactive UI on enter press

---
+++ Home/Assets/menupress.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menupress : MonoBehaviour
+{
+
+    public GameObject SelfUI;
+    public GameObject MenuButtonsUI;
+    public GameObject SubmitUI;
+
+    public void OnKeyEnter()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SelfUI.SetActive(false);
+            MenuButtonsUI.SetActive(true);
+            SubmitUI.SetActive(true);
+        }
+    }
+
+
+}
